07940e538e5924fec3dc4b842db50f04aa4e0efc
Mizuri <pro.1.h@outlook.com>
2022-11-28 16:12:22 -0700

    added event listener

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -32,7 +32,6 @@ export const putDb = async (content) => {
   console.log('Data saved to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
 
