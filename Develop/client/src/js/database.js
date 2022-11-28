9961e8cd9eb02f1cb239e601bceab6fca3e533be
Mizuri <pro.1.h@outlook.com>
2022-11-28 16:01:51 -0700

    added putDB code

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,8 +12,25 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+
+  //Create a connection to the database and version to use
+  const textDb=await openDB('text', 1);
+
+  //Create a new transaction and specify the database and data privileges
+  const tx = textDb.transaction('text', 'readwrite');
+
+  //Open up the desired object store
+  const store = tx.objectStore('text');
+
+  //Use the .add() method on the store and pass in the content
+  const request = store.put({ id: 1, value: content });
+
+  //Get confirmation of the request
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
