aef17706e8e13a0fe1f24ef86b8393637639f92e
Mizuri <pro.1.h@outlook.com>
2022-11-28 17:45:55 -0700

    updated install code

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,12 +2,8 @@ const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 window.addEventListener("beforeinstallprompt", (event) => {
-	console.log("hit");
-	console.log("event" + event);
-	event.preventDefault();
 	//Store the triggered events
 	window.deferredPrompt = event;
-
 	//Remove the hidden class from the button
 	butInstall.classList.toggle("hidden", false);
 });
@@ -19,18 +15,15 @@ butInstall.addEventListener("click", async () => {
 	if (!promptEvent) {
 		return;
 	}
-
 	//Show prompt
 	promptEvent.prompt();
-
 	//Reset the deferred prompt variable, it can only be used once
 	window.deferredPrompt = null;
-
 	butInstall.classList.toggle("hidden", true);
 });
 
 window.addEventListener("appinstalled", (event) => {
 	//Clear prompt
-	console.log("install hit");
 	window.deferredPrompt = null;
+    butInstall.classList.add('hidden')
 });
